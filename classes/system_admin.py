4daba72f0863b571092f3521c6fc1598108028c6
mendy hershkop <hers.mm@gmail.com>
2025-11-02 15:40:07 +0200

    crate System Admin class

--- classes/system_admin.py
+++ classes/system_admin.py
@@ -1,1 +1,10 @@
-ECHO is on.
+class SystemAdmin:
+    tota_transactions=0
+    @classmethod
+    def update_transactions_count(cls,amount:int=1) -> None:
+        cls.tota_transactions+=1
+    @classmethod
+    def report_stats(cls)->None:
+        print(cls.tota_transactions)
+        print(Library.max_borrow_days)
+
