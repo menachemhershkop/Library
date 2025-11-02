42eeb13c0ed0475ff3a7a8895d6c3a7baddf8660
mendy hershkop <hers.mm@gmail.com>
2025-11-02 15:29:41 +0200

    crate Logger class

--- classes/logger.py
+++ classes/logger.py
@@ -1,1 +1,6 @@
-ECHO is on.
+import time
+class Logger:
+    '''Log system anf jeneral cheks'''
+    def log_action(action_type:str, details:str)->None:
+        print(f'action type: {action_type}, detalis: {details} at {time.ctime()}')
+
