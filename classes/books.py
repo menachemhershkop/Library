1cda114ba79fbd11e42e8c9300448f0dae425715
mendy hershkop <hers.mm@gmail.com>
2025-11-02 14:59:00 +0200

    crate Book class

---
+++ classes/books.py
@@ -0,0 +1,9 @@
+class Book:
+    '''Books in the library'''
+    def __init__(self,title,author, isbn, is_available: bool = True):
+        self.title=str(title)
+        self.author=str(author)
+        self.isbn=str(isbn)
+        self.is_available=is_available
+    def get_derails(self)-> str:
+        return f'Name book: {self.title}, author: {self.author},number ID: {self.isbn} available: {self.is_available}'
